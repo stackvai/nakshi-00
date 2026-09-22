8988ade172b982c78081286dc5aa1236b06897e2
Abdul Baset <abdulbasetbappy.pro@gmail.com>
2026-09-22 20:52:24 +0600

    err

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', 'shadcn-nuxt','@vueuse/nuxt',],
-  shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: './components/ui'
-  },
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image','@vueuse/nuxt',],
+
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
